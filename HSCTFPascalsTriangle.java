8fa7213a1867212f5c496c5be42410c238e17898
killard2011 <amd41100@gmail.com>
2017-05-20 17:09:09 -0400

    Add files via upload
    Need to figure out how to get the value in the f(n, k) function

---
+++ HSCTFPascalsTriangle.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+// FIGURE OUT HOW TO DO THE PASCAL'S TRIANGLE PROBLEM
+public class Tester {
+
+	public final double q = (Math.pow(10, 9)) + 7;
+	public static void main(String[] args) {
+Tester();
+	}
+    
+	public static void Tester(){
+	
+		int n = 0;
+		int p = 0;
+		int k = 0;
+		Matth(n, p);
+
+	}
+    //The below function is the function for the f(n, k) in the pascal's triangle question in the HSCTF
+	public static int f(int n, int k){
+		
+		if(n >= k){
+			return 0;
+		}
+		
+		return 0;
+	}
+    //The below function is the Matth(n, p) function in the pascal's triangle question in the HSCTF
+	public static void Matth(int n, int p){
+		int num = 0;
+        //This for loop is going to be the sigma for the quesion.
+		for(int k = 0; k < 90000000; k++){
+			num = num + f(n, k);
+		}
+	}
+
+}
