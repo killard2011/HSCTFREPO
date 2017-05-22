b5be6b389b17df0d554850f7ea82cf15e3dee8a7
killard2011 <amd41100@gmail.com>
2017-05-22 10:49:06 -0500

    Update HSCTFPascalsTriangle.java
    I found the equation to get the kth term of the nth row

--- HSCTFPascalsTriangle.java
+++ HSCTFPascalsTriangle.java
@@ -3,37 +3,52 @@ import java.awt.*;
 import javax.swing.*;
 // FIGURE OUT HOW TO DO THE PASCAL'S TRIANGLE PROBLEM
 //Make the 10^20 # a BigInteger
-public class Tester {
+public class Test {
 
-	public final double q = (Math.pow(10, 9)) + 7;
+	public final static double q = (Math.pow(10, 9)) + 7;
 	public static void main(String[] args) {
 Tester();
 	}
     
 	public static void Tester(){
 	
-		int n = 0;
-		int p = 0;
+		int n = 50;
+		int p = 30;
 		int k = 0;
 		Matth(n, p);
-
+		//f(n, p);
 	}
+	
+	//ncr = (n!)/((r!)(n-r)!
+
     //The below function is the function for the f(n, k) in the pascal's triangle question in the HSCTF
 	public static int f(int n, int k){
-		
+		int val = 0;
 		if(n >= k){
-			return 0;
+		val =	(Factorial(n)/((Factorial(k)*(Factorial(n-k)))));
+		System.out.println(val);
+			return val;
 		}
 		
 		return 0;
 	}
+	public static int Factorial(int num){
+		int x = 1;
+		int val = 1;
+		while(x <= num){
+			val*=x;
+			x++;
+		}
+		return val;
+	}
     //The below function is the Matth(n, p) function in the pascal's triangle question in the HSCTF
-	public static void Matth(int n, int p){
+	public static int Matth(int n, int p){
 		int num = 0;
         //This for loop is going to be the sigma for the quesion.
 		for(int k = 0; k < 90000000; k++){
-			num = num + f(n, k);
+			num = num + f(n, k)*f(2*p, k);
 		}
+		return (int) (num%q);
 	}
 
 }
